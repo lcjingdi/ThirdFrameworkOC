d7b475fa63c30cfd5cf0b68d7153ace2efadb960
lcjingdi <lcjingdi@gmail.com>
2016-11-04 09:18:58 +0800

    初始化项目

---
+++ ThirdWebViewJavascriptBridgeDemo/ThirdWebViewJavascriptBridgeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirdWebViewJavascriptBridgeDemo
+//
+//  Created by jingdi on 2016/11/4.
+//  Copyright © 2016年 lcjingdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThirdWebViewJavascriptBridgeDemo/ThirdWebViewJavascriptBridgeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThirdWebViewJavascriptBridgeDemo
+//
+//  Created by jingdi on 2016/11/4.
+//  Copyright © 2016年 lcjingdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
