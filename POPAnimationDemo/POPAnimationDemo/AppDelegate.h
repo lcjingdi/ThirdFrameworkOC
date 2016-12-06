d9f26ea9b1a7c4c31284bbb9101e4f31099d4982
lcjingdi <lcjingdi@gmail.com>
2016-12-06 18:14:20 +0800

    增加了POPAnimationDemo工程

---
+++ POPAnimationDemo/POPAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  POPAnimationDemo
+//
+//  Created by jingdi on 2016/12/6.
+//  Copyright © 2016年 lcjingdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ POPAnimationDemo/POPAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  POPAnimationDemo
+//
+//  Created by jingdi on 2016/12/6.
+//  Copyright © 2016年 lcjingdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
